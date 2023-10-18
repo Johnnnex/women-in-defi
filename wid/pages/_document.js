a8904f4c0c7cf31d60a609e5a21f71a9a0e9ce5f
Johnnnex <onyeiborjohn05@gmail.com>
2023-10-18 17:14:13 +0100

    npm run

--- wid/pages/_document.js
+++ wid/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
