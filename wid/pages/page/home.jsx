b7f340ad72f2b4ecf7449dc8852ac0cfa3810d2d
Johnnnex <onyeiborjohn05@gmail.com>
2023-10-22 20:13:07 +0100

    testimonials

--- wid/pages/page/home.jsx
+++ wid/pages/page/home.jsx
@@ -186,7 +186,7 @@ const Homepage = () => {
           </figure>
         </section>
         <section className="py-[64px]">
-          <div className="max-w-[1272px] p-[24px] bg-[#0673EFCC] rounded-[24px] pt-[192px] w-[85%] mx-auto">
+          <div className="max-w-[1272px] flex justify-between px-[24px] bg-[#0673EFCC] rounded-[24px] pt-[92px] pb-[70px] w-[85%] mx-auto">
             <div className="">
               <h3 className="mb-[25px] text-white text-[32px] font-[600] leading-[42px]">Follow Us On Our Social Media</h3>
               <div className="flex gap-[24px] items-center">
@@ -221,8 +221,98 @@ const Homepage = () => {
                 </svg>
               </div>
             </div>
-            <div className="">
-              
+            <div className="grid-container">
+              <Image src="/images/img-1.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-2.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-3.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-4.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-5.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-6.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-7.png" height={1} width={50} alt="connect-images" />
+              <Image src="/images/img-8.png" height={1} width={50} alt="connect-images" />
+            </div>
+          </div>
+        </section>
+        <section className="pt-[98px] max-w-[1272px] w-[85%] mx-auto">
+          <h3 className="text-[32px] font-[500] mb-[60px] text-center leading-[42px]">
+            What WID <span className="text-[#0673EF]">Baddies</span> are Saying About Us
+          </h3>
+          <div className="flex gap-[24px]">
+            <div className=" flex-1">
+              <div className="p-[18px] md:mb-[40px] mb-[16px] bg-[#F9F9FF] rounded-[16px] h-fit">
+                <p className="mb-[24px] text-[#666481] text-[18px] font-[400]">
+                  WID changed my view about blockchain and crypto. A community blessed with great tutors that are patient in explaining and carrying their students along. My Defi journey has been a great one. It all started with #WID
+                </p>
+                <div className="flex gap-[8px] flex-col">
+                    <Image src="/images/img-7.png" alt="testimonial_avatar" height={1} width={50} />
+                    <p className="font-[500]">
+                      Nemi James
+                    </p>
+                </div>
+              </div>
+              <div className="p-[18px] bg-[#F9F9FF] rounded-[16px] h-fit">
+                <p className="mb-[24px] text-[#666481] text-[18px] font-[400]">
+                  Determination and WID TEAM are synonymous. Honestly, there’s a lot to say about WOMEN IN DEFI (WID) Community, the massive love the cooperation, and the active tutors are just what anyone can’t get elsewhere. I came in with Zero knowledge but today i can say am Good with What i Do. Defi is the future, joining a good community is the way forward.  <br />
+                  Thank you Miss Sarah for all the hard work you have put in place for the smooth running of the community.
+                </p>
+                <div className="flex gap-[8px] flex-col">
+                  <Image src="/images/img-1.png" alt="testimonial_avatar" height={1} width={50} />
+                  <p className="font-[500]">
+                    Thompson Dora
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className=" flex-1">
+                <div className="p-[18px] md:mb-[40px] mb-[16px] bg-[#F9F9FF] rounded-[16px] h-fit">
+                    <p className="mb-[24px] text-[#666481] text-[18px] font-[400]">
+                      Joining women in Defi was actually one of my best gift the lord gave me the year 2022. It changed my life and made me meet a lot of important people that i could call sisters. They really make the community look like family. I am grateful for having the best tutors also.
+                    </p>
+                    <div className="flex gap-[8px] flex-col">
+                      <Image src="/images/img-2.png" alt="testimonial_avatar" height={1} width={50} />
+                      <p className="font-[500]">
+                        Swizzabane Mide
+                      </p>
+                    </div>
+                </div>
+                <div className="p-[18px] bg-[#F9F9FF] rounded-[16px] h-fit">
+                    <p className="mb-[24px] text-[#666481] text-[18px] font-[400]">
+                      I was onboarded into the web3 space by the WID community in April this year; that was the best thing that happened to me.
+                      Before now, I had no knowledge of what web3 is or what the decentralized space is all about. <br />
+                      The only knowledge I had on crypto then was buying low and selling high 😅 <br />
+                      A big thank you to the founder of WomenInDefi and the core team members, WID was a game changer for me. Today, I'm an ardent believer in Decentralized Finance
+                    </p>
+                    <div className="flex gap-[8px] flex-col">
+                      <Image src="/images/img-3.png" alt="testimonial_avatar" height={1} width={50} />
+                      <p className="font-[500]">
+                        Vivian Adeniyi
+                      </p>
+                    </div>
+                </div>
+            </div>
+            <div className=" flex-1">
+                <div className="p-[18px] md:mb-[40px] mb-[16px] bg-[#F9F9FF] rounded-[16px] h-fit">
+                    <p className="mb-[24px] text-[#666481] text-[18px] font-[400]">
+                      The WID team has not only taught me about Defi but also help me stand my feet in the decentralized finance space through the process of close watch and constant follow-up even after the class. After the class, i have been able to get jobs that have given me funds to get gadgets to improve my tech journey, funds to process admission, and also my upkeep. A big thanks to the team for also reaching out and helping.
+                    </p>
+                    <div className="flex gap-[8px] flex-col">
+                      <Image src="/images/img-4.png" alt="testimonial_avatar" height={1} width={50} />
+                      <p className="font-[500]">
+                        Lady Benny
+                      </p>
+                    </div>
+                </div>
+                <div className="p-[18px] bg-[#F9F9FF] rounded-[16px] h-fit">
+                    <p className="mb-[24px] text-[#666481] text-[18px] font-[400]">
+                      WID was the start of my defi journey and the start of my millionaire journey. I discovered so many opportunities that i can leverage to make myself financially stable in the defi space. The biggest motivation is the fact that i’m taught by women who have broken records, which gives me the inspiration to do more. Then being part of a community that looks out for you can’t be overemphasized as it speeds up my growth.
+                    </p>
+                    <div className="flex gap-[8px] flex-col">
+                      <Image src="/images/img-5.png" alt="testimonial_avatar" height={1} width={50} />
+                      <p className="font-[500]">
+                        Uwadimma Christabel
+                      </p>
+                    </div>
+                </div>
             </div>
           </div>
         </section>
