c0c2c84fba5b17ffb5b4187aca660e3de5fbb2b5
Johnnnex <onyeiborjohn05@gmail.com>
2023-10-25 18:15:52 +0100

    patnership hero

--- wid/pages/page/donation.jsx
+++ wid/pages/page/donation.jsx
@@ -10,21 +10,43 @@ const Donation = () => {
         <section className="pt-[113px] pb-[99px] justify-between flex items-center max-w-[1272px] w-[85%] mx-auto">
           <div className="w-[49%]">
           <h3 className="mb-[16px] wow fadeInDown text-[#110138] font-[600] text-[40px] leading-[52px]">
-              Attend A WID Event
+            Partner With Our Community
           </h3>
           <p className="text-[#666481] wow fadeIn mb-[32px] text-[18px] font-[400] leading-[26px]">
-              Be a part of any of our online or offline events including twitter spaces, webinars, defi training, skill-up program, codes,charts and cocktails, and several more!
+            We partner with Web2/Web3 organizations to provide our community with access to events, protocols, and job opportunities.
           </p>
-          <button className="btn-generic wow fadeIn">View Upcoming Events</button>
+          <button className="btn-trans wow fadeIn">Partnership Inquiries</button>
           </div>
-          <figure className="relative wow bounceIn h-[491px] w-[45%] max-w-[540px]">
-            <Image 
-                src="/images/Frame-14.png" 
-                alt="section-img" 
-                priority 
+          <div className="wow bounceIn h-fit grid grid-cols-2 w-fit">
+            <figure className="relative w-[216px] h-[212px]">
+                <Image 
+                src="/images/donate-img-1.png" 
+                alt="hero-img"  
                 fill
-            />
-          </figure>
+              />
+            </figure>
+            <figure className="relative w-[216px] h-[212px]">
+                <Image 
+                src="/images/donate-img-2.png" 
+                alt="hero-img"  
+                fill
+              />
+            </figure>
+            <figure className="relative w-[216px] h-[212px]">
+                <Image 
+                src="/images/donate-img-3.png" 
+                alt="hero-img"  
+                fill
+              />
+            </figure>
+            <figure className="relative w-[216px] h-[212px]">
+              <Image 
+                src="/images/donate-img-4.png" 
+                alt="hero-img"  
+                fill
+              />              
+            </figure>
+          </div>
         </section>
       </main>
     </>
