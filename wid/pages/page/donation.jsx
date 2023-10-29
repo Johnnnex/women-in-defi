7ea08fb6a97894793e4ac2bff5caf37ecca92c6e
Johnnnex <onyeiborjohn05@gmail.com>
2023-10-29 14:11:49 +0100

    donations

--- wid/pages/page/donation.jsx
+++ wid/pages/page/donation.jsx
@@ -73,42 +73,42 @@ const Donation = () => {
             </p>
           </div>
         </section>
-        <section className="pt-[113px] pb-[99px] justify-between flex items-center max-w-[1272px] w-[85%] mx-auto">
-          <div className="w-[49%]">
-          <h3 className="mb-[16px] wow fadeInDown text-[#110138] font-[600] text-[40px] leading-[52px]">
+        <section className="md:pt-[113px] pt-[40px] pb-[29px] md:pb-[99px] md:justify-between flex flex-col md:flex-row gap-[38px] md:gap-0 md:items-center max-w-[1272px] md:w-[85%] w-[90%] mx-auto">
+          <div className="md:w-[49%]">
+          <h3 className="md:mb-[16px] mb-[10px] wow fadeInDown text-[#110138] font-[600] text-[24px] md:text-[40px] leading-[40px] md:leading-[52px]">
             Supporting The Women-In-Defi
           </h3>
-          <p className="text-[#666481] wow fadeIn mb-[32px] text-[18px] font-[400] leading-[26px]">
+          <p className="text-[#666481] wow fadeIn text-[16px] mb-[32px] md:text-[18px] font-[400] leading-[30px] md:leading-[26px]">
             Help sponsor our events and initiatives to empower more African women in Blockchain technology via a donation or more. <br />
             This donation will go a long way in supporting our numerous initiatives and facilitating our events and programs across several chapters. 
           </p>
           <button className="btn-trans wow fadeIn">Donate Now</button>
           </div>
-          <div className="wow bounceIn h-fit grid grid-cols-2 w-fit">
-            <figure className="relative w-[216px] h-[212px]">
+          <div className="wow bounceIn h-fit grid grid-cols-2 w-full md:w-fit">
+            <figure className="relative md:w-[216px] w-[100%] rounded-tl-[10px] overflow-hidden h-[212px]">
               <Image 
-                src="/images/donation-fr-1.png" 
+                src="/images/donation-fr-1.jpeg" 
                 alt="hero-img"  
                 fill
               />
             </figure>
-            <figure className="relative w-[216px] h-[212px]">
+            <figure className="relative md:w-[216px] w-[100%] rounded-tr-[10px] overflow-hidden h-[212px]">
               <Image 
-                src="/images/donation-fr-2.png" 
+                src="/images/donation-fr-2.jpeg" 
                 alt="hero-img"  
                 fill
               />
             </figure>
-            <figure className="relative w-[216px] h-[212px]">
+            <figure className="relative md:w-[216px] w-[100%] rounded-bl-[10px] overflow-hidden h-[212px]">
               <Image 
-                src="/images/donation-fr-3.png" 
+                src="/images/donation-fr-3.jpeg" 
                 alt="hero-img"  
                 fill
               />
             </figure>
-            <figure className="relative w-[216px] h-[212px]">
+            <figure className="relative md:w-[216px] w-[100%] rounded-br-[10px] overflow-hidden h-[212px]">
               <Image 
-                src="/images/donation-fr-4.png" 
+                src="/images/donation-fr-4.jpeg" 
                 alt="hero-img"  
                 fill
               />              
